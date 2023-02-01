85faf882660325927964f70e652aed1833429640
Isaac Tamara <isaactamara01@gmail.com>
2023-01-31 22:34:37 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,54 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+const userBD = []
+let baseId = 1
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Server OK'
+  })
+})
+
+app.post('/users', (req, res) => {
+  const data = req.body
+
+  const newUser = {    
+    "id": baseId++,
+    "firstName": data.firstName,
+    "lastName": data.lastName,
+    "email": data.email,
+    "password": data.password,
+    "age": data.age
+  }
+  userBD.push(newUser)
+  res.status(201).json(newUser)
+}) 
+
+app.get('/users', (req, res) => {
+  res.status(200).json(userBD)
+})
+
+app.get('/users/:id', (req, res) => {
+  const id = Number(req.params.id)
+
+  const data = userBD.find((item) => id === item.id)
+
+  if (data) {
+    res.status(200).json(
+      data
+    )   
+  }else {
+    res.status(404).json({
+      message: 'Invalid id'
+    })
+  }
+})
+
+app.listen(9000, () => {
+  console.log('server started at http://localhost:9000')
+})
 
 module.exports = app
